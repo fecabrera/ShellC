b6c90b60e509a21802ba3a47ca938d4e006312ce
Felipe Cabrera <necabrera@udec.cl>
2016-03-23 01:43:37 -0300

    Pipes funcionando

--- ShellC.h
+++ ShellC.h
@@ -23,9 +23,11 @@
 
 extern const char *dir_list[];
 
+void ShellError(const char *str);
+void CommandNotFound(const char *cmd);
 int parse(char *str, char *argv[], const char* delim);
 int get_dir(const char *home, const char *path, char *str);
-int run_cmd(char *argv[], int index, int npipes);
+int run_cmd(char *argv[], int index, int npipes, int *in_pipe, int *out_pipe);
 void cd(char *argv[]);
 
 #endif // __SHELL_C
